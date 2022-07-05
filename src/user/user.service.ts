53f13cc7088ebc275f9c3fbc9a02ceed395f9351
Max Koldun <koldun.maks@gmail.com>
2022-07-05 19:33:18 +0300

    Add patch for user, add tests for user and auth

---
+++ src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { EditUserDto } from './dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async editUser(userId: number, updatedUser: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: updatedUser,
+    });
+
+    delete user.hash;
+
+    return user;
+  }
+}
